f90a2e6f72ab16365301ecd1f3bf7c9c47cf31af
Karuhame <tomtran2k3@gmail.com>
2023-04-10 23:02:32 +0700

    initial commit

---
+++ PBL3/Controllers/AccountsController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using PBL3.Models;
+
+namespace PBL3.Controllers
+{
+    public class AccountsController : Controller
+    {
+        private PBL3Entities db = new PBL3Entities();
+
+        // GET: Accounts
+        public ActionResult Index()
+        {
+            var accounts = db.Accounts.Include(a => a.AccountInfo);
+            return View(accounts.ToList());
+        }
+
+        // GET: Accounts/Details/5
+        public ActionResult Details(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Account account = db.Accounts.Find(id);
+            if (account == null)
+            {
+                return HttpNotFound();
+            }
+            return View(account);
+        }
+
+        // GET: Accounts/Create
+        public ActionResult Create()
+        {
+            ViewBag.ACCOUNT_ID = new SelectList(db.AccountInfoes, "ACCOUNT_ID", "USER_NAME");
+            return View();
+        }
+
+        // POST: Accounts/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ACCOUNT_ID,ACCOUNT_NAME,ACCOUNT_PASSWORD,ACCOUNT_ROLE")] Account account)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Accounts.Add(account);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.ACCOUNT_ID = new SelectList(db.AccountInfoes, "ACCOUNT_ID", "USER_NAME", account.ACCOUNT_ID);
+            return View(account);
+        }
+
+        // GET: Accounts/Edit/5
+        public ActionResult Edit(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Account account = db.Accounts.Find(id);
+            if (account == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.ACCOUNT_ID = new SelectList(db.AccountInfoes, "ACCOUNT_ID", "USER_NAME", account.ACCOUNT_ID);
+            return View(account);
+        }
+
+        // POST: Accounts/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ACCOUNT_ID,ACCOUNT_NAME,ACCOUNT_PASSWORD,ACCOUNT_ROLE")] Account account)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(account).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.ACCOUNT_ID = new SelectList(db.AccountInfoes, "ACCOUNT_ID", "USER_NAME", account.ACCOUNT_ID);
+            return View(account);
+        }
+
+        // GET: Accounts/Delete/5
+        public ActionResult Delete(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Account account = db.Accounts.Find(id);
+            if (account == null)
+            {
+                return HttpNotFound();
+            }
+            return View(account);
+        }
+
+        // POST: Accounts/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(string id)
+        {
+            Account account = db.Accounts.Find(id);
+            db.Accounts.Remove(account);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ PBL3/Controllers/ThietBisController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using PBL3.Models;
+
+namespace PBL3.Controllers
+{
+    public class ThietBisController : Controller
+    {
+        private PBL3Entities db = new PBL3Entities();
+
+        // GET: ThietBis
+        public ActionResult Index()
+        {
+            return View(db.ThietBis.ToList());
+        }
+
+        // GET: ThietBis/Details/5
+        public ActionResult Details(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ThietBi thietBi = db.ThietBis.Find(id);
+            if (thietBi == null)
+            {
+                return HttpNotFound();
+            }
+            return View(thietBi);
+        }
+
+        // GET: ThietBis/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: ThietBis/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "THIETBI_ID,THIETBI_NAME,THIETBI_STATUS,THIETBI_NUM")] ThietBi thietBi)
+        {
+            if (ModelState.IsValid)
+            {
+                db.ThietBis.Add(thietBi);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(thietBi);
+        }
+
+        // GET: ThietBis/Edit/5
+        public ActionResult Edit(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ThietBi thietBi = db.ThietBis.Find(id);
+            if (thietBi == null)
+            {
+                return HttpNotFound();
+            }
+            return View(thietBi);
+        }
+
+        // POST: ThietBis/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "THIETBI_ID,THIETBI_NAME,THIETBI_STATUS,THIETBI_NUM")] ThietBi thietBi)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(thietBi).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(thietBi);
+        }
+
+        // GET: ThietBis/Delete/5
+        public ActionResult Delete(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ThietBi thietBi = db.ThietBis.Find(id);
+            if (thietBi == null)
+            {
+                return HttpNotFound();
+            }
+            return View(thietBi);
+        }
+
+        // POST: ThietBis/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(string id)
+        {
+            ThietBi thietBi = db.ThietBis.Find(id);
+            db.ThietBis.Remove(thietBi);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
