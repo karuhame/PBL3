13664f0c754931562f6a0ca428ac78a39e9c24c5
mimibetty <74227789+mimibetty@users.noreply.github.com>
2023-05-14 00:37:44 +0700

    Update AccountsController.cs
    Update AccountsController.cs

--- PBL3_2/Controllers/AccountsController.cs
+++ PBL3_2/Controllers/AccountsController.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Data;
 using System.Data.Entity;
@@ -6,6 +6,7 @@ using System.Linq;
 using System.Net;
 using System.Web;
 using System.Web.Mvc;
+using PagedList;
 using PBL3_2.Models;
 
 namespace PBL3_2.Controllers
@@ -15,11 +16,86 @@ namespace PBL3_2.Controllers
         private DBGym db = new DBGym();
 
         // GET: Accounts
-        public ActionResult Index()
+        public ActionResult Index(string strSearchThietBi, string SortOrder, string SortBy, int? page)
         {
-            var accounts = db.Accounts.Include(a => a.AccountInfo);
-            return View(accounts.ToList());
+
+            //ViewBag.strSearch = strSearch;
+
+            ViewBag.SortOrder = SortOrder;
+            ViewBag.SortBy = SortBy;
+            ViewBag.CurrentFilter = strSearchThietBi;
+
+            var obj = db.Accounts.ToList();
+
+            //Tìm kiếm
+            if (!String.IsNullOrEmpty(strSearchThietBi))
+            {
+                obj = obj.Where(p => p.ACCOUNT_NAME.Contains(strSearchThietBi)
+                                     || p.ACCOUNT_ROLE.ToString() == strSearchThietBi).ToList();
+            }
+
+            //Sắp xếp
+            switch (SortBy)
+            {
+                case "ACCOUNT_ROLE":
+                    {
+                        switch (SortOrder)
+                        {
+                            case "Asc":
+                                {
+                                    obj = obj.OrderBy(p => p.ACCOUNT_ROLE).ToList();
+                                    break;
+                                }
+                            default:
+                                {
+                                    obj = obj.OrderByDescending(p => p.ACCOUNT_ROLE).ToList();
+                                    break;
+                                }
+                        }
+                        break;
+                    }
+           
+
+            }
+            int pageSize = 5;
+            int pageNumber = (page ?? 1);
+            return View(obj.ToPagedList(pageNumber, pageSize));
+
         }
+
+        // GET: Accounts/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Account account = db.Accounts.Find(id);
+            if (account == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.ACCOUNT_NAME = new SelectList(db.AccountInfos, "ACCOUNT_NAME", "USER_NAME", account.ACCOUNT_NAME);
+            return View(account);
+        }
+
+        // POST: Accounts/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ACCOUNT_ID,ACCOUNT_NAME,ACCOUNT_PASSWORD,ACCOUNT_ROLE")] Account account)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(account).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.ACCOUNT_NAME = new SelectList(db.AccountInfos, "ACCOUNT_NAME", "USER_NAME", account.ACCOUNT_NAME);
+            return View(account);
+        }
+
 
         // GET: Accounts/Details/5
         public ActionResult Details(int? id)
@@ -61,38 +137,6 @@ namespace PBL3_2.Controllers
             return View(account);
         }
 
-        // GET: Accounts/Edit/5
-        public ActionResult Edit(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            Account account = db.Accounts.Find(id);
-            if (account == null)
-            {
-                return HttpNotFound();
-            }
-            ViewBag.ACCOUNT_NAME = new SelectList(db.AccountInfos, "ACCOUNT_NAME", "USER_NAME", account.ACCOUNT_NAME);
-            return View(account);
-        }
-
-        // POST: Accounts/Edit/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
-        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "ACCOUNT_ID,ACCOUNT_NAME,ACCOUNT_PASSWORD,ACCOUNT_ROLE")] Account account)
-        {
-            if (ModelState.IsValid)
-            {
-                db.Entry(account).State = EntityState.Modified;
-                db.SaveChanges();
-                return RedirectToAction("Index");
-            }
-            ViewBag.ACCOUNT_NAME = new SelectList(db.AccountInfos, "ACCOUNT_NAME", "USER_NAME", account.ACCOUNT_NAME);
-            return View(account);
-        }
 
         // GET: Accounts/Delete/5
         public ActionResult Delete(int? id)
