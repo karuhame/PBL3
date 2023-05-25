89393cb8e9cfb70b4bbd5b62779ee1df5c5e088b
karuhame <tomtran2k3@gmail.com>
2023-05-25 22:20:27 +0700

    BienLai

--- PBL3_2/Controllers/BienLaisController.cs
+++ PBL3_2/Controllers/BienLaisController.cs
@@ -18,23 +18,14 @@ namespace PBL3_2.Controllers
     {
         private DBGym db = new DBGym();
         // GET: BienLais
-        public ActionResult Index(string strSearchBienLai, string TenNguoiTraTien = "", int SortBy = 0, int SortOrder = 0)
+        public ActionResult Index( string TenNguoiTraTien = "", int SortBy = 0, int SortOrder = 0)
         {
             ViewBag.TenNguoiTraTien = TenNguoiTraTien;
             ViewBag.SortBy = SortBy;
             ViewBag.SortOrder = SortOrder;
-            ViewBag.CurrentFilter = strSearchBienLai;
 
             // Lap danh sach bien lai 
             List<BienLai> l = db.BienLais.ToList();
-
-            //Tìm kiếm
-            if (!String.IsNullOrEmpty(strSearchBienLai))
-            {
-                l= l.Where(p => p.Account.ACCOUNT_NAME.Contains(strSearchBienLai)).ToList();
-            }
-
-
 
 
             if (User.Identity.IsAuthenticated) // da dang nhap chua 
